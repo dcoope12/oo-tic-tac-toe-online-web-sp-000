56ad62fe860ed92b05f155f610c14d15a61e8f3c
Devin Cooper <dcoope12@asu.edu>
2020-09-24 01:32:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,7 +23,13 @@ puts "-----------"
 puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
+def input_to_index(user_input)
+user_input.to_i - 1
+end
 
+def move(index, current_player = "X")
+@board[index] = current_player
+end
 
 
 
